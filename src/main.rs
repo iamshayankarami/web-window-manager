22e3b652535c08fde0c41d4beabdef53e3684888
Iamshayankarami <ftfsk8203@gmail.com>
2020-12-31 00:20:15 +0330

    get starting with rust cargo and I have to work on it but I love it

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,23 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use] extern crate rocket;
 
+use rocket_contrib::templates::Template;
+
 #[get("/")]
 fn index() -> &'static str {
 "Hello, world!"
 }
 
+#[get("/<username>")]
+fn read_user_name(username: String) -> Template {
+    format!("Test {}", username.as_str())
+}
+
+#[post("/login", data="<username>, <password>")]
+fn login_form(username: Form<username>, password: Form<password>) -> Template {
+    Template::render("login", username, password)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, read_user_name]).launch();
 }
